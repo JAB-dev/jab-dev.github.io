858b4ebf7a9750838723c03874aa520d9fc9d390
JAB-dev <68183649+JAB-dev@users.noreply.github.com>
2021-01-10 20:17:46 +0200

    largely updated
    Made changes using xara as I intend to use github to host links to solutions I find useful

---
+++ index_html_files/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_5_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "Caption text...",
+ image: "index_html_files/221.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_html_files/222.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_html_files/223.jpg"
+},
+{
+ caption: "Caption text...",
+ image: "index_html_files/224.jpg"
+}
+];
---
+++ index_html_files/3.js
@@ -0,0 +1,38 @@
+window.local_simpleGallery3Columns_10_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_html_files/232.jpg",
+ thumbnail: "index_html_files/233.jpg"
+},
+{
+ image: "index_html_files/234.jpg",
+ thumbnail: "index_html_files/235.jpg"
+},
+{
+ image: "index_html_files/236.jpg",
+ thumbnail: "index_html_files/237.jpg"
+},
+{
+ image: "index_html_files/238.jpg",
+ thumbnail: "index_html_files/239.jpg"
+},
+{
+ image: "index_html_files/240.jpg",
+ thumbnail: "index_html_files/241.jpg"
+},
+{
+ image: "index_html_files/242.jpg",
+ thumbnail: "index_html_files/243.jpg"
+},
+{
+ image: "index_html_files/244.jpg",
+ thumbnail: "index_html_files/245.jpg"
+},
+{
+ image: "index_html_files/246.jpg",
+ thumbnail: "index_html_files/247.jpg"
+}
+];
